de5097bf6b88b915bc4b06565a29b15b2506e5d3
Aljaz200 <as67643@student.uni-lj.si>
2023-04-07 01:54:06 -0700

    V06

--- Vaje/vaje06/naloga.c
+++ Vaje/vaje06/naloga.c
@@ -7,22 +7,61 @@
 #include "naloga.h"
 
 int steviloZnakov(char* niz, char znak) {
-    // popravite / dopolnite ...
-    return -1;
+    int stevilo = 0;
+
+    for(int i = 0; niz[i] != '\0'; i++){
+        if(niz[i] == znak){
+            stevilo++;
+        }
+    }
+    return stevilo;
 }
 
 char* kopirajDoZnaka(char* niz, char znak) {
-    // popravite / dopolnite ...
-    return NULL;
+    int dolzina = 0;
+    for(dolzina; niz[dolzina] != '\0' && niz[dolzina] != znak; dolzina++);
+    char* kopija = (char*)calloc(dolzina, sizeof(char));
+    
+    for(int i = 0; i < dolzina; i++){
+        kopija[i] = niz[i];
+    }
+    kopija[dolzina] = '\0'; //nimam pojma zakaj???
+    return kopija;
+}
+
+char* kopirajDoZnakainzbrisi(char* niz, char znak, int pointer) {
+    int dolzina = 0;
+    for(dolzina; niz[dolzina+pointer] != '\0' && niz[dolzina+pointer] != znak; dolzina++);
+    char* kopija = (char*)calloc(dolzina, sizeof(char));
+    
+    for(int i = 0; i < dolzina; i++){
+        kopija[i] = niz[i+pointer];
+    }
+    kopija[dolzina] = '\0';
+    return kopija;
 }
 
 char** razcleni(char* niz, char locilo, int* stOdsekov) {
-    // popravite / dopolnite ...
-    return NULL;
+    *stOdsekov = steviloZnakov(niz, locilo) + 1;
+    char** tabela = calloc(*stOdsekov, sizeof(char*));
+    char* temp = niz;
+    int pointer = 0; 
+    for(int i = 0; i < *stOdsekov; i++){
+        tabela[i] = kopirajDoZnakainzbrisi(temp,locilo, pointer);
+        for(int j = pointer; temp[j] != '\0'; j++){
+            if(temp[j] == locilo){
+                pointer = j+1;
+                break;
+            }
+        }
+    }
+    return tabela;
 }
 
 int main() {
     // koda za ro"cno testiranje (po "zelji)
+    char* vtr = "eeeee";
+    printf("%d", steviloZnakov(vtr, 'e'));
 
     return 0;
 }
