9d447270d23b9387a49136fbdb381fe6105a9ff0
Aljaz200 <as67643@student.uni-lj.si>
2023-04-16 15:01:43 +0200

    Popravljena 2015 - 1. kolokvij - 3.naloga

--- Kolokviji/2015/1. kolokvij/3. naloga.c
+++ Kolokviji/2015/1. kolokvij/3. naloga.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int tabela[8] = {1,2,5,10,20,50,100,200};
+long tab[100000][8];
+int ZV[100000][8];
 
 long prestej(int znesek, int stevilo){
+	if(ZV[znesek][stevilo]){
+		return tab[znesek][stevilo];
+	}
     if(znesek == 0){
         return 1;
     }
@@ -12,9 +18,9 @@ long prestej(int znesek, int stevilo){
     if(stevilo == 8){
         return 0;
     }
-    long rez1 = prestej(znesek - tabela[stevilo], stevilo);
-    long rez2 = prestej(znesek, stevilo + 1);
-    return rez1 + rez2;
+    tab[znesek][stevilo]=  prestej(znesek - tabela[stevilo], stevilo) + prestej(znesek, stevilo + 1);
+    ZV[znesek][stevilo] = true;
+    return tab[znesek][stevilo];
 }
 
 int main(){
